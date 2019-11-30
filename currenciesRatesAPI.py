f7cdf1eda85dfec44f5141660b32044256544999
ElQuacko <programmer2be@gmail.com>
2019-12-01 00:47:06 +0100

    base_currency and exchange_currency tested and assigned by function

--- currenciesRatesAPI.py
+++ currenciesRatesAPI.py
@@ -1,23 +1,30 @@
 import requests
 
-currency_list = ['CAD', 'HKD', 'ISK', 'PHP', 'DKK', 'HUF', 'CZK', 'GBP', 'RON',
-'SEK', 'IDR', 'INR', 'BRL', 'RUB', 'HRK', 'JPY', 'THB', 'CHF', 'EUR', 'MYR',
-'BGN', 'TRY', 'CNY', 'NOK', 'NZD', 'ZAR', 'USD', 'MXN', 'SGD', 'AUD', 'ILS',
-'KRW', 'PLN']
+def currency_variable_assign():
+    currency_list = ['CAD', 'HKD', 'ISK', 'PHP', 'DKK', 'HUF', 'CZK', 'GBP', 'RON',
+    'SEK', 'IDR', 'INR', 'BRL', 'RUB', 'HRK', 'JPY', 'THB', 'CHF', 'EUR', 'MYR',
+    'BGN', 'TRY', 'CNY', 'NOK', 'NZD', 'ZAR', 'USD', 'MXN', 'SGD', 'AUD', 'ILS',
+    'KRW', 'PLN']
 
-while True:
-    base_currency = input('Enter base:')
-    control_variable = 0
-    for currency in range(len(currency_list)):
-        if base_currency == currency_list[currency]:
-            control_variable += 1
-    if control_variable == 1:
-        break
+    while True:
+        user_currency = input('Enter base:')
+        control_variable = False
+        for currency in range(len(currency_list)):
+            if user_currency == currency_list[currency]:
+                control_variable = True
+        if control_variable == True:
+            return user_currency
+            break
 
 #Finish it later!!!!
 def print_exchange_rate(base_currency):
     print('USD to %s exchange rate:' % base_currency, "%.2f" % round(1/rates_dictionary['USD'],2))
 
+base_currency = currency_variable_assign()
+print(base_currency)
+
+exchange_currency = currency_variable_assign()
+print(exchange_currency)
 
 API_url = ('https://api.exchangeratesapi.io/latest?base=' + base_currency)
 
@@ -35,7 +42,7 @@ rates_dictionary = response_dictionary['rates']
 #Test if converts properly PLN to PLN, should return 1
 #print(rates_dictionary['PLN'])
 
-
+"""
 print('\n')
 print('USD to %s exchange rate:' % base_currency, "%.2f" % round(1/rates_dictionary['USD'],2))
 print('EUR to %s exchange rate:' % base_currency, "%.2f" % round(1/rates_dictionary['EUR'],2))
@@ -43,5 +50,6 @@ print('GBP to %s exchange rate:' % base_currency, "%.2f" % round(1/rates_diction
 print('PLN to %s exchange rate:' % base_currency, "%.2f" % round(1/rates_dictionary['PLN'],2))
 print('JPY to %s exchange rate:' % base_currency, "%.2f" % round(1/rates_dictionary['JPY'],2))
 print('RUB to %s exchange rate:' % base_currency, "%.2f" % round(1/rates_dictionary['RUB'],2))
+"""
 
-print_exchange_rate(base_currency)
+#print_exchange_rate(base_currency)
