43aef0cf9995a0ce4893192d4ebc498749ac190b
ElQuacko <programmer2be@gmail.com>
2019-11-30 20:50:13 +0100

    API keys print & testing of accuracy

--- currenciesRatesAPI.py
+++ currenciesRatesAPI.py
@@ -9,3 +9,15 @@ print('Status code:', r.status_code)
 #Stores API response as json
 response_dictionary = r.json()
 print('Update date:', response_dictionary['date'])
+print('Base currency:', response_dictionary['base'])
+
+rates_dictionary = response_dictionary['rates']
+#print(rates_dictionary.keys())
+"""RETURNS
+dict_keys(['CAD', 'HKD', 'ISK', 'PHP', 'DKK', 'HUF', 'CZK', 'GBP', 'RON',
+'SEK', 'IDR', 'INR', 'BRL', 'RUB', 'HRK', 'JPY', 'THB', 'CHF', 'EUR', 'MYR',
+'BGN', 'TRY', 'CNY', 'NOK', 'NZD', 'ZAR', 'USD', 'MXN', 'SGD', 'AUD', 'ILS',
+'KRW', 'PLN'])
+"""
+#Test if converts properly PLN to PLN, should return 1
+#print(rates_dictionary['PLN'])
