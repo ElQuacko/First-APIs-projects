44513cd7679e16be9559b86da78e1001e0a990b5
ElQuacko <programmer2be@gmail.com>
2019-11-30 20:41:45 +0100

    Verifies status code & update date

--- currenciesRatesAPI.py
+++ currenciesRatesAPI.py
@@ -1,1 +1,11 @@
-print('hello')
+import requests
+
+API_url = 'https://api.exchangeratesapi.io/latest?base=PLN'
+
+r = requests.get(API_url)
+#Verifies if app works, if status code == 200 everything is great
+print('Status code:', r.status_code)
+
+#Stores API response as json
+response_dictionary = r.json()
+print('Update date:', response_dictionary['date'])
