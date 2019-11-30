c1f12b5d2e2cddbc9087a50baa1d5f5bf3bbb02d
ElQuacko <programmer2be@gmail.com>
2019-11-30 21:51:42 +0100

    Working base URL change, user_preferences not working yet

--- currenciesRatesAPI.py
+++ currenciesRatesAPI.py
@@ -1,6 +1,18 @@
 import requests
 
-API_url = 'https://api.exchangeratesapi.io/latest?base=PLN'
+def user_preferences(dict_keys):
+    while True:
+        base_choice = input('Select base currency:')
+        control_variable = 0
+        for choice in dict_keys:
+            if base_choice == choice['dict_keys']:
+                control_variable +=1
+        if control_variable == 1:
+            False
+
+
+base = input('Enter base:')
+API_url = ('https://api.exchangeratesapi.io/latest?base=' + base)
 
 r = requests.get(API_url)
 #Verifies if app works, if status code == 200 everything is great
@@ -12,6 +24,7 @@ print('Update date:', response_dictionary['date'])
 print('Base currency:', response_dictionary['base'])
 
 rates_dictionary = response_dictionary['rates']
+print(rates_dictionary)
 #print(rates_dictionary.keys())
 """RETURNS
 dict_keys(['CAD', 'HKD', 'ISK', 'PHP', 'DKK', 'HUF', 'CZK', 'GBP', 'RON',
@@ -22,9 +35,13 @@ dict_keys(['CAD', 'HKD', 'ISK', 'PHP', 'DKK', 'HUF', 'CZK', 'GBP', 'RON',
 #Test if converts properly PLN to PLN, should return 1
 #print(rates_dictionary['PLN'])
 
-print('\n\n')
+dict_keys = rates_dictionary.keys()
+
+print('\n')
 print('USD to PLN exchange rate:', "%.2f" % round(1/rates_dictionary['USD'],2))
 print('EUR to PLN exchange rate:', "%.2f" % round(1/rates_dictionary['EUR'],2))
 print('GBP to PLN exchange rate:', "%.2f" % round(1/rates_dictionary['GBP'],2))
 print('JPY to PLN exchange rate:', "%.2f" % round(1/rates_dictionary['JPY'],2))
 print('RUB to PLN exchange rate:', "%.2f" % round(1/rates_dictionary['RUB'],2))
+
+#user_preferences(dict_keys)
