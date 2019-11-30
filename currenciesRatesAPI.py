f716ee3c6195f7f6be3599db8d058bfcaf84642a
ElQuacko <programmer2be@gmail.com>
2019-11-30 20:57:54 +0100

    Prints 5 exchange rates with 2 decimal precision

--- currenciesRatesAPI.py
+++ currenciesRatesAPI.py
@@ -21,3 +21,10 @@ dict_keys(['CAD', 'HKD', 'ISK', 'PHP', 'DKK', 'HUF', 'CZK', 'GBP', 'RON',
 """
 #Test if converts properly PLN to PLN, should return 1
 #print(rates_dictionary['PLN'])
+
+print('\n\n')
+print('USD to PLN exchange rate:', "%.2f" % round(1/rates_dictionary['USD'],2))
+print('EUR to PLN exchange rate:', "%.2f" % round(1/rates_dictionary['EUR'],2))
+print('GBP to PLN exchange rate:', "%.2f" % round(1/rates_dictionary['GBP'],2))
+print('JPY to PLN exchange rate:', "%.2f" % round(1/rates_dictionary['JPY'],2))
+print('RUB to PLN exchange rate:', "%.2f" % round(1/rates_dictionary['RUB'],2))
