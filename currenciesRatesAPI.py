d33679a6a643d50d1f0e576abc6de31084d95c51
ElQuacko <programmer2be@gmail.com>
2019-12-01 01:00:33 +0100

    Cleaned, working program

--- currenciesRatesAPI.py
+++ currenciesRatesAPI.py
@@ -1,5 +1,6 @@
 import requests
 
+#Function used to assign currency variables and to verify if they are compatibile with the API
 def currency_variable_assign():
     currency_list = ['CAD', 'HKD', 'ISK', 'PHP', 'DKK', 'HUF', 'CZK', 'GBP', 'RON',
     'SEK', 'IDR', 'INR', 'BRL', 'RUB', 'HRK', 'JPY', 'THB', 'CHF', 'EUR', 'MYR',
@@ -7,7 +8,7 @@ def currency_variable_assign():
     'KRW', 'PLN']
 
     while True:
-        user_currency = input('Enter base:')
+        user_currency = input('Currency abbreviation:')
         control_variable = False
         for currency in range(len(currency_list)):
             if user_currency == currency_list[currency]:
@@ -15,32 +16,27 @@ def currency_variable_assign():
         if control_variable == True:
             return user_currency
             break
+        else:
+            print('Unknown currency abbraviation, enter once again!')
 
-#Finish it later!!!!
+#Function printing exchange rate for entered currencies
 def print_exchange_rate(base_currency, exchange_currency):
     print('%s' % base_currency + ' to %s exchange rate:' % exchange_currency, "%.2f" % round(1/rates_dictionary['%s' % exchange_currency],2))
 
 base_currency = currency_variable_assign()
-print(base_currency)
-
 exchange_currency = currency_variable_assign()
-print(exchange_currency)
 
+#API url, base_currency at the end is used to define base for the API
 API_url = ('https://api.exchangeratesapi.io/latest?base=' + base_currency)
 
 r = requests.get(API_url)
-#Verifies if app works, if status code == 200 everything is great
-print('Status code:', r.status_code)
 
 #Stores API response as json
 response_dictionary = r.json()
 print('Update date:', response_dictionary['date'])
-print('Base currency:', response_dictionary['base'])
 
+#Line of the code needed to start functions and print exchange rates
 rates_dictionary = response_dictionary['rates']
-
-#Test if converts properly PLN to PLN, should return 1
-#print(rates_dictionary['PLN'])
 
 """
 print('\n')
